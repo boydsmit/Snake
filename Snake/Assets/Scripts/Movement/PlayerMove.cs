612acbbf549f98c54420969d158b0628724cf3f9
Boyd smit <boydsmit@live.nl>
2018-11-06 14:05:17 +0100

    =Added basic variables for movement script

--- Snake/Assets/Scripts/Movement/PlayerMove.cs
+++ Snake/Assets/Scripts/Movement/PlayerMove.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : MonoBehaviour
+{
 
+	[SerializeField] private int _initialMoveSpd;
+	private int _curMoveSpd;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		_curMoveSpd = _initialMoveSpd;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 }
