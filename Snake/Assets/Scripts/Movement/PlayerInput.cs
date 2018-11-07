8159c89a366bc825444a6b8d006ca3b3427c4c3e
Boyd smit <boydsmit@live.nl>
2018-11-07 11:28:38 +0100

    =Finished MoveScript +Player InputScript

---
+++ Snake/Assets/Scripts/Movement/PlayerInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class PlayerInput : MonoBehaviour {
+
+	public enum Dir  {Left,Right,Up,Down}
+
+	public Dir CurDir;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetKey();
+	}
+
+	void GetKey()
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			CurDir = Dir.Left;
+		}
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			CurDir = Dir.Up;
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			CurDir = Dir.Right;
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			CurDir = Dir.Down;
+		}
+	}
+
+	
+}
--- Snake/Assets/Scripts/Movement/PlayerMove.cs
+++ Snake/Assets/Scripts/Movement/PlayerMove.cs
@@ -8,17 +8,49 @@ public class PlayerMove : MonoBehaviour
 
 	[SerializeField] private int _initialMoveSpd;
 	private int _curMoveSpd;
+	private Rigidbody _rb;
+
+	private PlayerInput _input;
 	
 	// Use this for initialization
 	void Start ()
 	{
+		_rb = gameObject.GetComponent<Rigidbody>();
 		_curMoveSpd = _initialMoveSpd;
+		_input = gameObject.GetComponent<PlayerInput>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		GetDir();			
 	}
+
+	void Move(float x, float y)
+	{
+		_rb.velocity = new Vector2(x, y).normalized * _curMoveSpd * Time.deltaTime;
+	}
+
+	void GetDir()
+	{
+		switch (_input.CurDir)
+		{
+			case PlayerInput.Dir.Up:
+				Move(0,1);
+				break;
+			
+			case PlayerInput.Dir.Down:
+				Move(0,-1);
+				break;
+			case PlayerInput.Dir.Left:
+				Move(-1,0);
+				break;
+			
+			case PlayerInput.Dir.Right:
+				Move(1,0);
+				break;
+		}
+	}
+
 }
 	
